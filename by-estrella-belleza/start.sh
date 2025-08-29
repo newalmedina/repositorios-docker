389ef38ae56c38bf2af946482ed841aa0a90085b
IM3-238\im3-div6 <newal.medina@ingenieros-im3.com>
2025-08-29 22:16:39 +0200

    no message

--- by-estrella-belleza/start.sh
+++ by-estrella-belleza/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# PHP-FPM ya se lanza como proceso propio
-# Si quieres correrlo con supervisord, no necesitas esta línea
-
 # Iniciar supervisord (cron, queue workers, etc.) en foreground
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n
-
-# Mantener el contenedor vivo
-# tail -f /var/log/supervisor/*.log
